bfa15ac02731b771ec61da229bb53166351d56b2
Róbert <rob.iskola1@gmail.com>
2021-03-01 10:05:46 +0100

    privát scanneres verzio

---
+++ 4.het/ArrayListTest/src/szavak/SzoBeolvas2.java
@@ -0,0 +1,30 @@
+package szavak;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class SzoBeolvas2 {
+    private static Scanner input = new Scanner(System.in); 
+
+    static ArrayList<String> szavakatEltarol() {
+        ArrayList<String> szavak = new ArrayList<>();
+        System.out.println("Add meg a szavakat: (üres szora leall a program)");
+
+        String szo = input.nextLine();
+        do {
+            szavak.add(szo);
+            szo = input.nextLine();
+        } while(!szo.isEmpty());
+
+        return szavak;
+    }
+
+    static void kiir(ArrayList<String> arr) {
+        for(String s : arr) 
+            System.out.println(s);
+    }
+    public static void main(String[] args) {   
+        ArrayList<String> szavak = szavakatEltarol();
+        kiir(szavak);
+    }
+}
