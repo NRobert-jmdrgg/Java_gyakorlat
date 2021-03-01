8c5822199c0eaf8b20a987ccf801e055601ef6d4
Róbert <rob.iskola1@gmail.com>
2021-03-01 13:17:02 +0100

    alkalmazott tomb

--- 4.het/Munkahely/src/muka/Munkahely.java
+++ 4.het/Munkahely/src/muka/Munkahely.java
@@ -1,9 +1,18 @@
 package muka;
 
+import java.util.Random;
+
 public class Munkahely {
     public static void main(String[] args) {
-        Alkalmazott a = new Alkalmazott("Pisti");
-        a.increaseSalary(4000);
-        
+        final int meret = 5;
+        final String[] nevek = {"Aladár", "Bence", "Cecil", "Dávid", "Elemér"};
+        Random r = new Random();
+        Alkalmazott[] alkalmazottak = new Alkalmazott[meret];
+
+        for(int i = 0; i < meret; i++) {
+            alkalmazottak[i] = new Alkalmazott(nevek[i], r.nextInt(1000000 - 470000) + 470000);
+        }
     }
+
+    
 }
