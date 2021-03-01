8ff476062b18dd92eb94bce3b2553822e21b23ee
Róbert <rob.iskola1@gmail.com>
2021-03-01 09:25:00 +0100

    Scanner method

---
+++ 4.het/ArrayListTest/src/szavak/szoBeolvas.java
@@ -0,0 +1,13 @@
+package szavak;
+
+import java.util.Scanner;
+
+public class szoBeolvas {
+    static Scanner megnyitInput() {
+        return new Scanner(System.in);
+    }
+    public static void main(String[] args) {
+        Scanner input = megnyitInput();
+        
+    }
+}
