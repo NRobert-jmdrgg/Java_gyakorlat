b9797a0b676f7d144a550bdfef20322256bc8098
Róbert <rob.iskola1@gmail.com>
2021-03-01 09:28:50 +0100

    sorbeolvas es arraylist letrehozva

--- 4.het/ArrayListTest/src/szavak/szoBeolvas.java
+++ 4.het/ArrayListTest/src/szavak/szoBeolvas.java
@@ -1,13 +1,18 @@
 package szavak;
 
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class szoBeolvas {
     static Scanner megnyitInput() {
         return new Scanner(System.in);
     }
+    static String sorBeolvas(Scanner in) {
+        return in.nextLine();
+    }
+
     public static void main(String[] args) {
         Scanner input = megnyitInput();
+        ArrayList<String> szavak = new ArrayList<>();
         
     }
 }
