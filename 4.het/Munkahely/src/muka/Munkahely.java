1aba15d7b5ea8096af21c8743a67a6f3a1c181d9
Róbert <rob.iskola1@gmail.com>
2021-03-01 13:21:40 +0100

    legnagyobb fizetessel rendelkezo

--- 4.het/Munkahely/src/muka/Munkahely.java
+++ 4.het/Munkahely/src/muka/Munkahely.java
@@ -3,6 +3,15 @@ package muka;
 import java.util.Random;
 
 public class Munkahely {
+    public static void legnagyobbFizetesselRendelkezo(final Alkalmazott[] alk) {
+        int maxind = 0;
+        for(int i = 0 ; i < alk.length; i++) {
+            if(alk[i].getSalary() > alk[maxind].getSalary()) {
+                maxind = i;
+            }
+        }
+        System.out.println(alk[maxind].getName() + " -nak van a legnagyobb fizetese");
+    }
     public static void main(String[] args) {
         final int meret = 5;
         final String[] nevek = {"Aladár", "Bence", "Cecil", "Dávid", "Elemér"};
@@ -14,5 +23,5 @@ public class Munkahely {
         }
     }
 
-    
+
 }
