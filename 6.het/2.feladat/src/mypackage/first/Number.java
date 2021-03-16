bbb3d0563f13da47893ddeadb0863b584e26e9a1
Róbert <rob.iskola1@gmail.com>
2021-03-16 23:31:15 +0100

    finish me later

---
+++ 6.het/2.feladat/src/mypackage/first/Number.java
@@ -0,0 +1,16 @@
+package mypackage.first;
+
+public class Number {
+    private static final float TURESHATAR = 0.001;
+    private float valosSzam;
+
+    public Number(float valosSzam) {
+        this.valosSzam = valosSzam;
+    }
+
+    public boolean turesenBelul(float num) {
+        return 
+    }
+
+    
+}
