a4c77dc7263144dd972b86f88c6c9cabf5863908
'Róbert <'rob.iskola1@gmail.com'>
2021-03-22 19:40:19 +0100

    teglalap class

--- Sikidom/src/sikidom/Teglalap.java
+++ Sikidom/src/sikidom/Teglalap.java
@@ -1,10 +1,56 @@
 package sikidom;
 
 public class Teglalap {
-	private int sidea;
+	private int sideA;
 	private int sideB;
 	
+	public Teglalap(int sidea, int sideB) {
+		super();
+		this.sideA = sidea;
+		this.sideB = sideB;
+	}
 	
+	public Teglalap(int bothSides) {
+		super();
+		this.sideA = bothSides;
+		this.sideB = bothSides;
+	}
 	
+	public int getArea() {
+		return this.sideA * this.sideB;
+	}
+
+	@Override
+	public String toString() {
+		return "Teglalap [sideA=" + sideA + ", sideB=" + sideB + ", Area=" + getArea() + "]";
+	}
+
+	public int getSideA() {
+		return sideA;
+	}
+
+	public void setSideA(int sideA) {
+		this.sideA = sideA;
+	}
+
+	public int getSideB() {
+		return sideB;
+	}
+
+	public void setSideB(int sideB) {
+		this.sideB = sideB;
+	}
 	
+	public void setBothSides(int bothSides) {
+		this.sideA = bothSides;
+		this.sideB = bothSides;
+	}
+	
+	public boolean biggerThan(Teglalap t) {
+		return (this.getArea() > t.getArea());
+	}
+	
+	public boolean isEqualTo(Teglalap t) {
+		 return (this.sideA == t.sideA && this.sideB == t.sideB);
+	 }
 }
