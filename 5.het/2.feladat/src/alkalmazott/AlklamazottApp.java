346ccaa25a80cbdfb03105604aa9df3788f77571
Róbert <rob.iskola1@gmail.com>
2021-03-22 08:56:26 +0100

    alkalmazott fuggvenyek

--- 5.het/2.feladat/src/alkalmazott/AlklamazottApp.java
+++ 5.het/2.feladat/src/alkalmazott/AlklamazottApp.java
@@ -4,10 +4,28 @@ package alkalmazott;
  */
 public class AlklamazottApp {
     public static void main(String[] args) {
-        Alkalmazott a = new Alkalmazott("Pista", 400000);
-        a.setSalary(99999999);
-        System.out.println(a);
-        a.increaseSalary(40);
-        System.out.println(a);
+        Alkalmazott[] alkalmazottak = new Alkalmazott[5];
+        feltolt(alkalmazottak);
+        kiirAlkalmazottak(alkalmazottak);
+    }
+
+    public static void feltolt(Alkalmazott[] alk) {
+        for (int i = 0; i < alk.length; i++) {
+            alk[i] = new Alkalmazott("bela " + (i + 1) , i + 10000);
+        }
+    }
+
+    public static void kiirAlkalmazottak(Alkalmazott[] alk) {
+        for (Alkalmazott alkalmazott : alk) {
+            System.out.println(alkalmazott);
+        }
+    }
+
+    public static double altagFizetes(Alkalmazott[] alk) {
+        int sum = 0;
+        for (Alkalmazott alkalmazott : alk) {
+            sum += alkalmazott.getSalary();
+        }
+        return (double)sum / alk.length;
     }
 }
