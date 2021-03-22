ebdc95709edb5cf4e42adfcb884922e33e4f6da3
Róbert <rob.iskola1@gmail.com>
2021-03-22 09:51:12 +0100

    fuggvenyek kijavitva

--- 5.het/2.feladat/src/alkalmazott/Bolt.java
+++ 5.het/2.feladat/src/alkalmazott/Bolt.java
@@ -24,33 +24,5 @@ public class Bolt {
         return boltiAlkalmazottak;
     }
 
-    private static double altagFizetes(Alkalmazott[] alk) {
-        int sum = 0;
-        for (Alkalmazott alkalmazott : alk) {
-            sum += alkalmazott.getSalary();
-        }
-        return (double)sum / alk.length;
-    }
-
-    private static int sjaOsszeg(Alkalmazott[] alk) {
-        int sum = 0;
-        for (Alkalmazott alkalmazott : alk) {
-            sum += alkalmazott.getSJA();
-        }
-
-        return sum;
-    }
-
-    private static Alkalmazott legnagyobbFizetes(Alkalmazott[] a) {
-        Alkalmazott max = a[0];
-        for (int i = 1; i < a.length; i++) {
-            if (max.nagyobbE(a[i])) {
-                max = a[i];
-            }
-        }
-        return max;
-    }
-
-
-
+    
 }
