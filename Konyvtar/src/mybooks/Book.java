5699713e2fe6eec5a6f8fd68a903e825d99742b5
'Róbert <'rob.iskola1@gmail.com'>
2021-03-22 19:28:03 +0100

    book osztaly

--- Konyvtar/src/mybooks/Book.java
+++ Konyvtar/src/mybooks/Book.java
@@ -1,5 +1,31 @@
 package mybooks;
 
 public class Book {
+	private String title;
+	private String author;
+	private int    yearOfPublication;
+	private int    price;
+	
+	public Book(String title, String author, int yearOfPublication, int price) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.yearOfPublication = yearOfPublication;
+		this.price = price;
+	}
+	
+	public void increaseByPercent(int percent) {
+		this.price = this.price * ((100 + percent) / 100); 
+	}
+
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", author=" + author + ", yearOfPublication=" + yearOfPublication + ", price="
+				+ price + "]";
+	}
+	
+	
+	
+	
 	
 }
