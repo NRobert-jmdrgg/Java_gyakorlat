f6488ce404252774db60fbb37b949e0f4c797f2a
'Róbert <'rob.iskola1@gmail.com'>
2021-03-22 18:53:41 +0100

    alkalmazott osztaly

--- Alkalmazott/src/munkahely/Alkalmazott.java
+++ Alkalmazott/src/munkahely/Alkalmazott.java
@@ -1,6 +1,19 @@
 package munkahely;
 
 public class Alkalmazott {
+	private String name;
+	private int    salary;
+	
+	public void increaseSalary(int bonus) {
+		this.salary += bonus;
+	}
 
+	@Override
+	public String toString() {
+		return "Alkalmazott [name=" + name + ", salary=" + salary + "]";
+	}
+	
+	
+	
 	
 }
