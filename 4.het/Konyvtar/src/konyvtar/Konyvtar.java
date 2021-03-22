fd5751259b219fa004c06ed85298edd6719b6760
Róbert <rob.iskola1@gmail.com>
2021-03-22 11:48:01 +0100

    rendbe lesz rakva

--- 4.het/Konyvtar/src/konyvtar/Konyvtar.java
+++
@@ -1,8 +0,0 @@
-package konyvtar;
-
-public class Konyvtar {
-    public static void main(String[] args) {
-        Konyv k = new Konyv("Cim", "Szerzo");
-        System.out.println(k.toString());
-    }
-}
--- 5.het/2feladatjomegoldas/src/alkalmazott/AlkalmazottApp.java
+++
@@ -1,9 +0,0 @@
-package alkalmazott;
-
-public class AlkalmazottApp {
-    public static void main(String[] args) {
-        Alkalmazott a = new Alkalmazott("Karcsi", 1000000);
-        System.out.println(a.toString());
-        System.out.println(Alkalmazott.getEmployeeCount());
-    }
-}
