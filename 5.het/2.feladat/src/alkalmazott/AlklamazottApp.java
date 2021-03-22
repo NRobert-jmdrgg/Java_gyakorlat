a4d528f82b36caf17fc03cbe7247eb3ececdfa50
Róbert <rob.iskola1@gmail.com>
2021-03-22 09:34:24 +0100

    feltolt athelyezve

--- 5.het/2.feladat/src/alkalmazott/AlklamazottApp.java
+++ 5.het/2.feladat/src/alkalmazott/AlklamazottApp.java
@@ -5,18 +5,14 @@ package alkalmazott;
 public class AlklamazottApp {
     public static void main(String[] args) {
         Alkalmazott[] alkalmazottak = new Alkalmazott[5];
-        feltolt(alkalmazottak);
+        
         kiirAlkalmazottak(alkalmazottak);
         System.out.println(altagFizetes(alkalmazottak));
         System.out.println(legnagyobbFizetes(alkalmazottak));
         System.out.println(sjaOsszeg(alkalmazottak));
     }
 
-    private static void feltolt(Alkalmazott[] alk) {
-        for (int i = 0; i < alk.length; i++) {
-            alk[i] = new Alkalmazott("bela " + (i + 1) , i + 10000);
-        }
-    }
+    
 
     private static void kiirAlkalmazottak(Alkalmazott[] alk) {
         for (Alkalmazott alkalmazott : alk) {
--- 5.het/2.feladat/src/alkalmazott/Bolt.java
+++ 5.het/2.feladat/src/alkalmazott/Bolt.java
@@ -1,5 +1,17 @@
 package alkalmazott;
 
 public class Bolt {
-    private Alkalmazott[] = new Alkalmazott[10];
+    private int letszam;
+    private Alkalmazott[] boltiAlkalmazottak;
+
+    public Bolt(int letszam) {
+        this.letszam = letszam;
+        this.boltiAlkalmazottak = new Alkalmazott[letszam];
+    }
+
+    private static void feltolt(Alkalmazott[] alk) {
+        for (int i = 0; i < alk.length; i++) {
+            alk[i] = new Alkalmazott("bela " + (i + 1) , i + 10000);
+        }
+    }
 }
