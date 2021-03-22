2f4c415eacff771e32c18eadabd29da98d9666c0
'Róbert <'rob.iskola1@gmail.com'>
2021-03-22 15:40:01 +0100

    Matrix - 1.gyakorlat 4. feladata

---
+++ Matrix/src/matrixok/MatrixApp.java
@@ -0,0 +1,30 @@
+package matrixok;
+
+public class MatrixApp {
+
+	public static void main(String[] args) {
+		int[][] mx = new int[4][4];
+		for(int i = 0; i < 4; i++) 
+			for(int j = 0; j < 4; j++) mx[i][j] = j;
+		
+		matrixKiir(mx);
+		System.out.println(matrixKeres(mx, 3));
+	}
+	
+	static void matrixKiir(int mx[][]) {
+		for(int i = 0; i < mx.length; i++) {
+			for(int j = 0; j < mx[0].length; j++) System.out.print(mx[i][j] + " ");
+			System.out.println();
+		}	
+	}
+	
+	static boolean matrixKeres(int mx[][], int key) {
+		for(int i = 0; i < mx.length; i++) 
+			for(int j = 0; j < mx[0].length; j++) 
+				if(mx[i][j] == key) return true;
+			
+		
+		return false;
+	}
+
+}
