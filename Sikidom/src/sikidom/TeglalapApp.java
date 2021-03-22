0d89f67e12834839796768447600d920f9991cdd
'Róbert <'rob.iskola1@gmail.com'>
2021-03-22 20:09:11 +0100

    fuggvenyek

--- Sikidom/src/sikidom/TeglalapApp.java
+++ Sikidom/src/sikidom/TeglalapApp.java
@@ -1,28 +1,80 @@
 package sikidom;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class TeglalapApp {
 	
+	private static Random r = new Random();
+	private static Scanner s = new Scanner(System.in);
+	
+	
+	private static void feltolt(Teglalap[] t) {
+		for (int i = 0; i < t.length; i++) {
+			t[i] = new Teglalap((r.nextInt(10 - 2) + 1 - 2), (r.nextInt(10 - 2) + 1 - 2));
+			
+		}
+	}
+	
+	private static void legkissebbTeglalapAdatai(Teglalap[] t) {
+		int min = 0;
+		for (int i = 0; i < t.length; i++) {
+			if (t[i].getArea() < t[min].getArea()) {
+				min = i;
+			}
+			
+		}
+		System.out.println(t[min]);
+	}
+	
+	private static int getInt() {
+		int a;
+		System.out.println("Szam: ");
+		while (!s.hasNextInt()) {
+			System.out.println("Nem szam");
+			s.next();
+		}
+		a = s.nextInt();
+		s.nextLine();
+		return a;
+	}
+	
+	private static Teglalap teglalapBeker() {
+		System.out.println("adj meg egy teglalapot");
+		int a = getInt();
+		int b = getInt();
+		return new Teglalap(a, b);
+	}
+	
+	private static int nagyobbTeglalapokSzama(Teglalap[] teglalapok, Teglalap t) {
+		int sum = 0;
+		for (Teglalap teglalap : teglalapok) {
+			if (teglalap.getArea() > t.getArea()) {
+				sum++;
+			}
+		}
+		return sum; 
+	}
+	
+	private static void elsoEgyenlo(Teglalap[] teglalapok, Teglalap t) {
+		for (Teglalap teglalap : teglalapok) {
+			if (teglalap.isEqualTo(t)) {
+				System.out.println(teglalap);
+				break;
+			}
+		}
+		System.out.println("nincs ilyen");
+	}
+	
 	public static void main(String[] args) {
-		Teglalap a = new Teglalap(5);
-		Teglalap b = new Teglalap(2, 4);
+		Teglalap[] t = new Teglalap[10];
+		feltolt(t);
 		
-		Teglalap c = a;
 		
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(c);
-		
-		a.setSideA(5);
-		a.setSideB(7);
-		
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(c);
-		
-		a = b;
-		System.out.println((a == b));
-		System.out.println((a == c));
-		System.out.println(a.isEqualTo(b));
+		legkissebbTeglalapAdatai(t);
+		Teglalap myTeglalap = teglalapBeker();
+		System.out.println(nagyobbTeglalapokSzama(t, myTeglalap));
+		elsoEgyenlo(t, myTeglalap);
 		
 	}
 
