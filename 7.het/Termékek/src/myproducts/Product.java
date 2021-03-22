72c0ee5de28dfae20ef86b3e6f7af9c9520022b7
Róbert <rob.iskola1@gmail.com>
2021-03-22 10:11:33 +0100

    init

---
+++ 7.het/Termékek/src/myproducts/Product.java
@@ -0,0 +1,15 @@
+package myproducts;
+
+public class Product {
+    private String nev;
+    private int ar;
+    private int afakulcs;
+    
+    public Product(String nev, int ar, int afakulcs) {
+        this.nev = nev;
+        this.ar = ar;
+        this.afakulcs = afakulcs;
+    }
+
+    
+}
