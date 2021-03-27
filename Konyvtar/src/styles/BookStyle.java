d62de1e81b1728039e08fe0bf8076b779925ae51
'Róbert <'rob.iskola1@gmail.com'>
2021-03-27 19:38:31 +0100

    konyvstilus

---
+++ Konyvtar/src/styles/BookStyle.java
@@ -0,0 +1,21 @@
+package styles;
+
+import mybooks.*;
+
+public class BookStyle {
+	private String style;
+
+	public BookStyle(String style) {
+		super();
+		this.style = style;
+	}
+
+	public String getStyle() {
+		return style;
+	}
+	
+	
+	
+	
+	
+}
