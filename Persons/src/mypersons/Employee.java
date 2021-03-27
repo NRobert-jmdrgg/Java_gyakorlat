eae45fe99e286e4974403b96625caabbf52f0868
'Róbert <'rob.iskola1@gmail.com'>
2021-03-27 21:55:45 +0100

    alkalmazott

---
+++ Persons/src/mypersons/Employee.java
@@ -0,0 +1,24 @@
+package mypersons;
+
+public class Employee extends Adult {
+	private int salary;
+	private static int retirementAge = 65;
+	
+	public Employee(String name, int age, String workplace, int salary) {
+		super(name, age, workplace);
+		this.salary = salary;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [salary=" + salary + ", name=" + name + ", age=" + age + ", getSalary()=" + getSalary()
+				+ ", getWorkplace()=" + getWorkplace() + "]";
+	}
+	
+	
+	
+}
