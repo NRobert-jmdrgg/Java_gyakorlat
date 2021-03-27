d9a5a3b4d219c2654d6da0907177b65420b2a420
'Róbert <'rob.iskola1@gmail.com'>
2021-03-27 20:10:29 +0100

    beolvasas

--- Konyvtar/src/styles/alpackage/BookStyleApp.java
+++ Konyvtar/src/styles/alpackage/BookStyleApp.java
@@ -1,13 +1,58 @@
 package styles.alpackage;
 
 import styles.BookStyle;
+import java.util.Scanner;
 
 public class BookStyleApp {
-
-	public static void main(String[] args) {
-		BookStyle b = new BookStyle("Szerzo", "Cim", 2001, 24124, "jo");
+	
+	private static Scanner scan = new Scanner(System.in);
+	
+	private static int readInt() {
+		int a;
+		do {
+			while (!scan.hasNextInt()) {
+				System.out.println("Nem szam!!");
+				scan.next();
+			}
+			a = scan.nextInt();
+			scan.nextLine();
+		} while (a < 1 || a > 10);
+		return a;
+	}
+	
+	private static BookStyle readBookStyle() {
+		String author;
+		String title;
+		int year;
+		int price;
+		String style;
 		
-
+		System.out.println("Szerzo: ");
+		author = scan.nextLine();
+		System.out.println("cim: ");
+		title = scan.nextLine();
+		System.out.println("publikacio eve: ");
+		year = scan.nextInt();
+		scan.nextLine();
+		System.out.println("ar: ");
+		price = scan.nextInt();
+		scan.nextLine();
+		System.out.println("Stilus: ");
+		style = scan.nextLine();
+		
+		return new BookStyle(title, author, year, price, style);
+	}
+	
+	public static void main(String[] args) {
+		int n = readInt();
+		
+		BookStyle[] bs = new BookStyle[n];
+		
+		for (int i = 0; i < n; i++) {
+			bs[i] = readBookStyle();
+		}
+		
+		
 	}
 
 }
