5a6e5b2c83489fb3e5896bf5b55a06eaf6c9ffcc
'Róbert <'rob.iskola1@gmail.com'>
2021-03-27 20:52:44 +0100

    szemely beolvaso

--- Persons/src/mypersons/test/PersonTest.java
+++ Persons/src/mypersons/test/PersonTest.java
@@ -1,10 +1,28 @@
 package mypersons.test;
 
+import mypersons.*;
+import java.util.Scanner;
+
 public class PersonTest {
 
+	private static Scanner scan = new Scanner(System.in);
+	
+	private static Person readPerson() {
+		String name;
+		int age;
+		System.out.println("Nev:");
+		name = scan.nextLine();
+		System.out.println("eletkor: ");
+		age = scan.nextInt();
+		scan.nextLine();
+		
+		return new Person(name, age);
+	}
+	
 	public static void main(String[] args) {
-		Person p1;
-		Person p2;
+		Person p1 = readPerson();
+		Person p2 = readPerson();
+		
 	}
 
 }
