2bcada3b25e534598373a02c1549d1aeff053c84
'Róbert <'rob.iskola1@gmail.com'>
2021-03-27 19:08:18 +0100

    konstruktor

--- Products/src/myproducts/Product.java
+++ Products/src/myproducts/Product.java
@@ -1,5 +1,27 @@
 package myproducts;
 
 public class Product {
+	private String name;
+	private int nettoAr;
+	private int afaKulcs;
+	
+	public Product(String name, int nettoAr, int afaKulcs) {
+		super();
+		this.name = name;
+		this.nettoAr = nettoAr;
+		this.afaKulcs = afaKulcs;
+	}
+	
+	public double getBruttoAr() {
+		return this.nettoAr * (1 + this.afaKulcs/ 100);
+	}
 
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", nettoAr=" + nettoAr + ", afaKulcs=" + afaKulcs + ", getBruttoAr()="
+				+ getBruttoAr() + "]";
+	}
+	
+	
+	
 }
