74068c247883891de1c8308c2a9be2efb429b6e3
'Róbert <'rob.iskola1@gmail.com'>
2021-03-29 10:45:06 +0200

    b) feladat

--- Alkalmazott/src/hatodikhet/Alkalmazott.java
+++ Alkalmazott/src/hatodikhet/Alkalmazott.java
@@ -1,31 +1,66 @@
 package hatodikhet;
 
+import java.time.LocalDate;
+
 public class Alkalmazott {
-	private String name;
-	private int    age;
-	private int    salary;
+	private String 		 name;
+	private LocalDate    birthday;
+	private int    		 salary;
 	
-	private static int retirementAge = 65;
+	private static final int retirementAge = 65;
+	private static LocalDate currentDate = LocalDate.now();
+	private static String[] monthNames = {
+			" ",
+			"Januar",
+			"Februar",
+			"Marcius",
+			"Aprilis",
+			"Majus",
+			"Junius",
+			"Julius",
+			"Augusztus",
+			"Szeptember",
+			"Oktober",
+			"November",
+			"December"
+	};
 	
-	@Override
-	public String toString() {
-		return "Alkalmazott [name=" + name + ", age=" + age + ", salary=" + salary + ", yearsUntilRetirement()="
-				+ yearsUntilRetirement() + "]";
-	}
-
-	public Alkalmazott(String name, int age) {
-		this(name, age, 10000*age);
-	}
-
-	public Alkalmazott(String name, int age, int salary) {
-		super();
+	public Alkalmazott(String name, int year, int month, int day) {
 		this.name = name;
-		this.age = age;
-		this.salary = salary;
+		this.birthday = LocalDate.of(year, month, day);
+		this.salary = 10000*(currentDate.getYear() - birthday.getYear()); 
 	}
+	
+	public Alkalmazott(String name, int year, String month, int day) {
+		this.name = name;Y
+		int monthValue;
+		if (convertMonthName(month) == 0) {
+			monthValue = 1;
+		} else {
+			monthValue = convertMonthName(month);
+		}
+		this.birthday = LocalDate.of(year, monthValue, day);
+		this.salary = 10000*(currentDate.getYear() - birthday.getYear()); 
+	}
+
+	
+	
+	public String convertMonthValue(int m) {
+		return monthNames[m];
+	}
+	
+	public int convertMonthName(String m) {
+		for (int i = 1; i < monthNames.length; i++) {
+			if (monthNames[i].equalsIgnoreCase(m)) {
+				return i;
+			}	
+		}
+		return 0;
+	}
+	
 	
 	public int yearsUntilRetirement() {
-		return retirementAge - this.age;
+		return retirementAge - currentDate.getYear() - birthday.getYear();
 	}
 	
 	public int getSalary() {
@@ -42,9 +77,7 @@ public class Alkalmazott {
 		}
 	}
 
-	public static void setRetirementAge(int r) {
-		retirementAge = r;
-	}
+	
 	
 	
 	
