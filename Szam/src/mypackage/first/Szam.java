5d5edff7aaf09f555e8a8f8e73aaf8edc093ba9d
'Róbert <'rob.iskola1@gmail.com'>
2021-03-29 10:55:39 +0200

    .

---
+++ Szam/src/mypackage/first/Szam.java
@@ -0,0 +1,15 @@
+package mypackage.first;
+
+public class Szam {
+	private static final double THRESHOLD = 0.001;
+	private double num;
+	
+	public Szam(double num) {
+		super();
+		this.num = num;
+	}
+	
+	//unalmas feladat 
+	
+	
+}
