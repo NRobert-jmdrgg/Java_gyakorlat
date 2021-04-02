cbe5a6c9227aa6bdb5487444ff9091aef9b51f3c
'Róbert <'rob.iskola1@gmail.com'>
2021-04-02 21:32:53 +0200

    mycolor class

---
+++ Products/src/colorRunner/MyColor.java
@@ -0,0 +1,16 @@
+package colorRunner;
+
+import java.awt.Color;
+
+public class MyColor extends Color {
+	public MyColor(Color color) {
+		super(color.getRed(), color.getGreen(), color.getBlue());
+	}
+
+	@Override
+	public String toString() {
+		//TODO else if ág az összes színnek a világon.
+	}
+	
+	
+}
